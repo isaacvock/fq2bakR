cecf70a2bf5161440c9a18d0140881fe1346b15a
iwv2 <isaac.vock@yale.edu>
2022-09-19 15:10:35 -0400

    Proper handling of sample name wildcards

---
+++ workflow/scripts/hisat2.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Source the paths and variables:
+
+cpus=$1 # number of cpus
+sample=$2 # sample names
+format=$3 # Paired or single end
+reads=$4 # FR or RF
+chr_tag=$5
+HISAT_INDEX=$6
+
+if [ "$format" = "PE" ]; then
+    input1=$7
+    input2=$8
+    output=$9
+
+else
+    input=$7
+    output=$8
+fi
+-
+
+# align trimmed reads to the  genome
+    echo "* Aligning reads with HISAT2 for sample $sample"
+
+    if [[ "$chr_tag" = "TRUE" ]]; then
+        echo "* chr tag will be included in alignment for " $sample
+    fi
+
+
+    if [[ "$reads" = "FR" ]]; then
+       hisat2 \
+            -p "$cpus" \
+            -x $HISAT_INDEX \
+            -1 "$input1" \
+            -2 "$input2" \
+            $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
+            --rna-strandness FR \
+            --mp 4,2 \
+            -S "$sample".sam
+    elif [[ "$reads" = "RF" ]]; then
+        hisat2 \
+            -p "$cpus" \
+            -x $HISAT_INDEX \
+            -1 "$input1" \
+            -2 "$input2" \
+            $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
+            --rna-strandness FR \
+            --mp 4,2 \
+            -S "$sample".sam
+    elif [[ "$reads" = "F" ]]; then
+        hisat2 \
+            -p "$cpus" \
+            -x $HISAT_INDEX \
+            -U "$input" \
+            $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
+            --mp 4,2 \
+            -S "$sample".sam
+    else
+        echo "! No FR/RF/F method recognized for " $sample
+        exit 1
+    fi &&
+
+
+
+
+    samtools view -@ "$cpus" -o "$output" "$sample".sam
+
+    rm "$sample".sam
+
+    echo "* Alignment script finished for " $sample
--- workflow/scripts/preprocess_all.sh
+++ workflow/scripts/preprocess_all.sh
@@ -14,23 +14,17 @@
 
 
 
-if ["$format" = "PE"]; then
+if [ "$format" = "PE" ]; then
 
     output1=$5 # output1
     output2=$6
 
-    # Copy .fastq or .fastq.gz files into dir and create individual r1 and r2 fastq files:
-
-    if [[ -f $(echo ${fastq_dir}/*.fastq | cut -f 1 -d " ") ]]; then
-        suffix="fastq"
-    elif [[ -f $(echo ${fastq_dir}/*.gz | cut -f 1 -d " ") ]]; then
-        suffix="gz"
-    else
-        echo "!!! No fastq files found at location ${fastq_dir}"
-        exit 1
-    fi
-
-    fastqs=($(ls ${fastq_dir}))
+    # Create array of fastq file names
+    declare -a fastqs
+    for file in ${fastq_dir}/*.fastq
+    do
+        fastqs=(${fastqs[*]} "$file")
+    done
 
     # remove duplicate fastq reads:
             fastuniq \
@@ -62,83 +56,32 @@ if ["$format" = "PE"]; then
             rm "$sample"_2u.fastq
 
 
-elif ["$format" = "SE"]; then
+elif [ "$format" = "SE" ]; then
 
     output1=$5 # output1
 
-    # Copy .fastq or .fastq.gz files into dir and create individual r1:
-        if [ "$step_copyfq" = "TRUE" ]; then
-            # Check that files exist
-            if [[ -f $(echo ${LINK_BASE}/${prefix}${sample}/*.fastq | cut -f 1 -d " ") ]]; then
-                suffix="fastq"
-            elif [[ -f $(echo ${LINK_BASE}/${prefix}${sample}/*.gz | cut -f 1 -d " ") ]]; then
-                suffix="gz"
-            else
-                echo "!!! No fastq files found at location ${LINK_BASE}/${prefix}${sample}"
-                exit 1
-            fi
+    # Create array of fastq file names
+    declare -a fastqs
+    for file in ${fastq_dir}/*.fastq
+    do
+        fastqs=(${fastqs[*]} "$file")
+    done
 
-
-            # parallel copying of files
-            if [[ -n $GNUPARALLEL_MOD ]]; then module load ${GNUPARALLEL_MOD}; fi
-
-            parallel -j $cpus "cp {1} ./" :::  ${LINK_BASE}/${prefix}${sample}/*.${suffix}
-
-            echo "* .${suffix} files copied for sample $sample"
-
-
-            # Parallel decompression
-            if [[ $suffix == "gz" ]]; then
-                if [[ -n $PIGZ_MOD ]]; then module load ${PIGZ_MOD}; fi
-
-                $PIGZ -d -p "$cpus" *.gz &&
-                echo "* .fastq files decompressed for sample $sample"
-            fi
-
-            cat *R1* > "$sample"_1.fastq
-
-            rm -f *R1*
-
-        else
-            echo "* Skipping copy fastq step"
-        fi
-
-
-        echo "* Skipping fastuniq step for SE data"
-        in="$sample"_1.fastq
-
-
-        if [ "$step_cutadapt" = "TRUE" ]; then
+    # trim reads
             echo "* Running cutadapt in parallel mode for sample $sample"
 
-            if [[ -n $CUTADAPT_MOD ]]; then module load ${CUTADAPT_MOD}; fi
 
-            if [ "$STL" = "TRUE" ]; then
-                $CUTADAPT \
-                    -a ^CGATC...GATCGGAAGAGC \
-                    -a ^CGATC...GGAAGAGCACAC \
-                    -n 2 \
-                    --minimum-length=20 \
-                    --cores="$cpus" \
-                    -o "$sample".t.fastq \
-                    "$in" &&
-                echo "* cutadapt finished for " $sample
-            else
-                $CUTADAPT \
+                cutadapt \
                     -a AGATCGGAAGAGC \
                     --minimum-length=20 \
                     --cores="$cpus" \
-                    -o "$sample".t.fastq \
-                    "$in" &&
+                    -o "$output1" \
+                    "$sample"_1u.fastq &&
                 echo "* cutadapt finished for " $sample
-            fi
 
 
-            rm $in
-        else
-            echo "* Skipping cutadapt step"
-        fi
 
+            rm "$sample"_1u.fastq
 else
     echo "!!! format must be PE or SE !!!"
 fi
