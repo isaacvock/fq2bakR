27660a53764084233e0fc9f9f1f643ed2b83088f
iwv2 <isaac.vock@yale.edu>
2023-04-07 09:24:00 -0400

    Brought hisat2 up to speed

--- workflow/scripts/hisat2.sh
+++ workflow/scripts/hisat2.sh
@@ -5,7 +5,7 @@
 cpus=$1 # number of cpus
 sample=$2 # sample names
 format=$3 # Paired or single end
-reads=$4 # FR or RF
+reads=$4 # F or R
 chr_tag=$5
 HISAT_INDEX=$6
 
@@ -13,10 +13,12 @@ if [ "$format" = "PE" ]; then
     input1=$7
     input2=$8
     output=$9
+    outpu2=${10}
 
 else
     input=$7
     output=$8
+    output2=$9
 fi
 
 
@@ -28,44 +30,57 @@ fi
     fi
 
 
-    if [[ "$reads" = "FR" ]]; then
-       hisat2 \
-            -p "$cpus" \
-            -x $HISAT_INDEX \
-            -1 "$input1" \
-            -2 "$input2" \
-            $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
-            --rna-strandness FR \
-            --mp 4,2 \
-            -S "$sample".sam
-    elif [[ "$reads" = "RF" ]]; then
-        hisat2 \
-            -p "$cpus" \
-            -x $HISAT_INDEX \
-            -1 "$input2" \
-            -2 "$input1" \
-            $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
-            --rna-strandness FR \
-            --mp 4,2 \
-            -S "$sample".sam
-    elif [[ "$reads" = "F" ]]; then
-        hisat2 \
-            -p "$cpus" \
-            -x $HISAT_INDEX \
-            -U "$input" \
-            $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
-            --mp 4,2 \
-            -S "$sample".sam
+    if [[ "$format" = "PE" ]]; then
+        if [[ "$reads" = "F"]]; then
+        
+            hisat2 \
+                -p "$cpus" \
+                -x $HISAT_INDEX \
+                -1 "$input1" \
+                -2 "$input2" \
+                $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
+                --rna-strandness FR \
+                --mp 4,2 \
+                -S "$output2"
+        else
+            hisat2 \
+                -p "$cpus" \
+                -x $HISAT_INDEX \
+                -1 "$input1" \
+                -2 "$input2" \
+                $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
+                --rna-strandness FR \
+                --mp 4,2 \
+                -S "$output2"
+
+    elif [[ "$format" = "SE" ]]; then
+        if [[ "$reads" = "F"]]; then
+
+            hisat2 \
+                -p "$cpus" \
+                -x $HISAT_INDEX \
+                -1 "$input1" \
+                -2 "$input2" \
+                $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
+                --rna-strandness FR \
+                --mp 4,2 \
+                -S "$output2"
+        else
+
+            hisat2 \
+                -p "$cpus" \
+                -x $HISAT_INDEX \
+                -1 "$input1" \
+                -2 "$input2" \
+                $( if [ $chr_tag = 'TRUE' ]; then echo "--add-chrname "; fi ) \
+                --rna-strandness FR \
+                --mp 4,2 \
+                -S "$output2"
     else
-        echo "! No FR/RF/F method recognized for " $sample
+        echo "! No PE/SE FORMAT parameter recognized for " $sample
         exit 1
     fi &&
 
-
-
-
-    samtools view -@ "$cpus" -o "$output" "$sample".sam
-
-    rm "$sample".sam
+    samtools view -@ "$cpus" -o "$output" "$output2"
 
     echo "* Alignment script finished for " $sample
