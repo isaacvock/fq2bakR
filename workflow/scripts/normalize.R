5a04668b1ec79af71dbe7d3165b86d177d46ce52
iwv2 <isaac.vock@yale.edu>
2023-04-14 13:45:39 -0400

    Fixed normalization directory typo

--- workflow/scripts/normalize.R
+++ workflow/scripts/normalize.R
@@ -55,7 +55,7 @@ args = commandArgs(trailingOnly = TRUE)
 	# dirs <- opt$dirs %>% str_split(',') %>% unlist()
 
 	# Screw it, going to hardcode directory cause I can
-	dirs <- c(getwd(), "/results/bams/")
+	dirs <- c(getwd(), "/results/htseq/")
     paste(dirs)
 
         samplefiles <- list.files(path = dirs,
